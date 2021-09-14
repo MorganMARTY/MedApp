37524b4320a77e771fab14dda64739bd1946cd9e
JulieLeone <72926299+JulieLeone@users.noreply.github.com>
2021-09-14 17:43:16 +0200

    Informations patient

--- MedApp/src/app/app.component.ts
+++ MedApp/src/app/app.component.ts
@@ -13,13 +13,18 @@ export class AppComponent {
   server = "https://fhir.eole-consulting.io/api";
   pageMed: boolean = false;
   practitioner: any = {};
+  patient: any = {};
 
 constructor(private http: HttpClient){
   this.getPractitioner();
+  this.getPatient();
 }
 
 
 getPractitioner(){
   this.http.get(this.server + '/practitioner/613f4b25a5b46400122cf50f').subscribe(data => { this.practitioner = data });    
+}
+getPatient(){
+  this.http.get(this.server + '/patient/612e0350a5b46400122dx508').subscribe(data => { this.patient = data });    
 }
 }
