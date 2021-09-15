511e23be11cd1148e04178d29d25e7c2da923c1c
Marienrs <marienourrisson@orange.fr>
2021-09-15 17:37:20 +0200

    Page Patient

--- MedApp/src/app/app.component.ts
+++ MedApp/src/app/app.component.ts
@@ -12,11 +12,12 @@ export class AppComponent {
   title = 'MedApp';
   server = "https://fhir.eole-consulting.io/api";
   pageMed: boolean = false;
+  pagePatient: boolean = false;
   practitioner: any = {};
   patient: any = {};
   observationpatients: any = {};
   appointmentmed: any = {};
-
+  p: any = {};
   snackBar: any;
 
   constructor(private http: HttpClient) {
@@ -43,5 +44,9 @@ export class AppComponent {
     console.log("jsonRequest", JSON.stringify(this.practitioner));
     this.http.put(this.server + '/practitioner/613f4b25a5b46400122cf50f', this.practitioner).subscribe(data => { this.practitioner = data });
     console.log("response", this.practitioner);
+  }
+
+pagePat(referencePatient: string) {
+    this.http.get(this.server + '/'+ referencePatient.toLowerCase()).subscribe(data => { this.p = data });
   }
 }
