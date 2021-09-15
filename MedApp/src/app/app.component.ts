1e49f743b48ad9af606bea355840410f91178d44
MARTY Morgan <morgan.marty@etud.univ-jfc.fr>
2021-09-15 13:18:40 +0200

    Put médecin

--- MedApp/src/app/app.component.ts
+++ MedApp/src/app/app.component.ts
@@ -14,22 +14,33 @@ export class AppComponent {
   pageMed: boolean = false;
   practitioner: any = {};
   patient: any = {};
-  observationpatients: any={};
+  observationpatients: any = {};
 
-constructor(private http: HttpClient){
-  this.getPractitioner();
-  this.getPatient();
-  this.getObservation();
-}
+  snackBar: any;
+
+  constructor(private http: HttpClient) {
+    this.getPractitioner();
+    this.getPatient();
+    this.getObservation();
+  }
 
 
-getPractitioner(){
-  this.http.get(this.server + '/practitioner/613f4b25a5b46400122cf50f').subscribe(data => { this.practitioner = data });    
-}
-getPatient(){
-  this.http.get(this.server + '/patient/612e0350a5b46400122dx5081').subscribe(data => { this.patient = data });    
-}
-getObservation(){
-  this.http.get(this.server + '/observation?subject.reference=Patient/612e0350a5b46400122dx508').subscribe(data => { this.observationpatients = data });    
-}
+  getPractitioner() {
+    this.http.get(this.server + '/practitioner/613f4b25a5b46400122cf50f').subscribe(data => { this.practitioner = data });
+  }
+  getPatient() {
+    this.http.get(this.server + '/patient/612e0350a5b46400122dx508').subscribe(data => { this.patient = data });
+  }
+  getObservation() {
+    this.http.get(this.server + '/observation?subject.reference=Patient/612e0350a5b46400122dx508').subscribe(data => { this.observationpatients = data });
+  }
+  putPractitioner() {
+    console.log("jsonRequest", JSON.stringify(this.practitioner));
+    this.http.put(this.server + '/practitioner/613f4b25a5b46400122cf50f', this.practitioner).subscribe(data => { this.practitioner = data });
+    console.log("response", this.practitioner);
+    //this.popMessage("Vous avez bien mis à jour vos informations")
+  }
+ //popMessage(message: string): void {
+ //   this.snackBar.open(message, "", { duration: 2000 })
+  //}
 }
